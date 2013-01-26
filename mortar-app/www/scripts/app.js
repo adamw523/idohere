a4f948561ec3773c6dd488d718b0a5d576deb72e
Adam Wisniewski <adamw@tbcn.ca>
2013-01-26 12:38:06 -0500

    adding mortar template

---
+++ mortar-app/www/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var appApp = angular.module('appApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
